86aa4076ac92372a7f941661296e2622538dfc49
Ying Jie Mei <93160834+jackyx00@users.noreply.github.com>
2023-10-24 17:17:43 -0400

    added checklocationpermission

--- components/home/homescreen/HomeMapScreen.jsx
+++ components/home/homescreen/HomeMapScreen.jsx
@@ -1,52 +1,72 @@
-
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import styles from './homemapscreen.styles';
-import Image from 'react-native';
-import { SIZES } from '../../../constants'
-import Input from 'postcss/lib/input';
-import { Component } from 'react/cjs/react.production.min';
+import MapView from 'react-native-maps';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 //import { MapsComponent } from '@syncfusion/ej2-react-maps';
 //import Geolocation from '@react-native-community/geolocation';
 // Imports
-import MapView from 'react-native-maps';
 
-const HomeMapScreen = ({navigation}) => {
+const HomeMapScreen = ({ navigation }) => {
+    useEffect(() => {
+      checkLocationPermission();
+    }, []);
+  
+    const checkLocationPermission = async () => {
+      const locationPermission = await AsyncStorage.getItem('locationPermission');
+      if (locationPermission === 'granted') {
+      } else {
+        Alert.alert(
+          'Location Tracking Permission',
+          'Allow location tracking for customers?',
+          [
+            {
+              text: 'Do It Later',
+              style: 'cancel',
+            },
+            {
+              text: 'Yes',
+              onPress: async () => {
+                // Store the user's location
+                // geolocation here
+  
+                // Store the permission status
+                await AsyncStorage.setItem('locationPermission', 'granted');
+              },
+            },
+          ]
+        );
+      }
+    };
+  
     return (
-    <View style = {{flex:1, backgroundColor: '#efcb4e'} }>
+      <View style={{ flex: 1, backgroundColor: '#efcb4e' }}>
         <MapView
-        //Hunter College Coordinates
-            style={{ flex: 1 }}
-            initialRegion={{
-            latitude:  40.7861,
+          // Hunter College Coordinates
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 40.7861,
             longitude: -73.9543,
             latitudeDelta: 0.03,
             longitudeDelta: 0.02,
-            }}
-        > 
+          }}
+        >
         </MapView>
-
-        <View style = {styles.searchContainer}>
-            <View style = {styles.searchWrapper}>
-                <TextInput
-                style= {styles.searchInput}
-                value = ""
-                onChange = { () => {  } }
-                placeholder= "What are you looking for?"
-                placeholderTextColor="#888"
-            />  
-            </View>
-        
+  
+        <View style={styles.searchContainer}>
+          <View style={styles.searchWrapper}>
+            <TextInput
+              style={styles.searchInput}
+              value=""
+              onChange={() => {}}
+              placeholder="What are you looking for?"
+              placeholderTextColor="#888"
+            />
+          </View>
         </View>
-
-        
-    </View>
-
-    
-   
-    )
-
-
-};
-export default HomeMapScreen;
+      </View>
+    );
+  };
+  
+  export default HomeMapScreen;
