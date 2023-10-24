f207cf982a146b9e61f47a7480c2bc8e4e95af7d
Ying Jie Mei <93160834+jackyx00@users.noreply.github.com>
2023-10-24 15:50:14 -0400

    update to login screens

--- components/home/login/VendorLoginView.jsx
+++ components/home/login/VendorLoginView.jsx
@@ -1,65 +1,83 @@
-import { View, Text, ScrollView, Button, SafeAreaView, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, Button, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native'; // Import Alert
 import React, { useState } from 'react';
 import styles from './loginView.styles';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
-const VendorLoginView = ({navigation}) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const VendorLoginView = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    handleLogin = () => {
-        auth()
-     .signInWithEmailAndPassword(email, password)
-     .then(() => navigation.navigate('HomeMapScreen', {name:'HomeMapScreen'}))
-     .catch(error => {
-        if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
-        }
-    
-        console.error(error);
-      });
+  const handleLogin = async () => {
+    if (!email || !password) {
+        Alert.alert('Missing Fields', 'Please enter both email and password.');
+        return;
     }
 
-    return (
+    try {
+      const userCredential = await auth().signInWithEmailAndPassword(email, password);
+      const user = userCredential.user;
 
-        <View style = {styles.container}> 
+      const userDoc = await firestore()
+        .collection('Users')
+        .doc('dFqhRhGV5BSuqWYys6bP')
+        .collection('Vendors')
+        .doc(user.uid)
+        .get();
 
-            <Text style={styles.name}>
-                Street{'\n'} Savor 
-            </Text>
-        <View>    
-            <TextInput
-                style={[styles.input, {marginTop:30}]}
-                value={email}
-                onChangeText={setEmail}
-                placeholder="Enter email address"
-                autoCapitalize="none"/>
-            <TextInput
-                style={[styles.input, {marginTop:10}]}
-                value={password}
-                onChangeText={setPassword}
-                placeholder="Enter password"
-                autoCapitalize="none"
-                secureTextEntry/>
+      if (userDoc.exists) {
+        navigation.navigate('HomeMapScreen', { name: 'HomeMapScreen' });
+      } else {
+        setError('Invalid email or password. Please try again.');
+      }
+    } catch (error) {
+      if (error.code === 'auth/invalid-email') {
+        setError('Invalid email address.');
+        Alert.alert('Invalid Email', 'The email address you entered is not valid. Please try again.');
+      } else if (error.code === 'auth/internal-error') {
+        setError('Incorrect password. Please try again.');
+        Alert.alert('Password Incorrect', 'The password you entered is incorrect. Please try again.');
+      } else {
+        setError('An error occurred. Please try again.');
+      }
+    }
+  };
 
-            <TouchableOpacity style={[styles.button, {marginTop:20}]}
-                onPress={handleLogin}>
-                <Text style= {styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-            
-            <View style={[styles.textContainer, {marginTop:20}]}>
-            <Text style={styles.text}>Don't have an account? </Text>
-            <TouchableOpacity
-                onPress = { () => navigation.navigate('VendorSignupView', {name:'VendorSignupView'}) }>
-                <Text style= {styles.text2}>Sign Up</Text>
-            </TouchableOpacity>
-            </View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>Street{'\n'} Savor</Text>
+      <View>
+        <TextInput
+          style={[styles.input, { marginTop: 30 }]}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Enter email address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={[styles.input, { marginTop: 10 }]}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Enter password"
+          autoCapitalize="none"
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={[styles.button, { marginTop: 20 }]} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <View style={[styles.textContainer, { marginTop: 20 }]}>
+          <Text style={styles.text}>Don't have an account? </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('VendorSignupView', { name: 'VendorSignupView' })}>
+            <Text style={styles.text2}>Sign Up</Text>
+          </TouchableOpacity>
         </View>
-        </View>
-    )
-} 
-
-
-
+      </View>
+    </View>
+  );
+};
 
 export default VendorLoginView;
