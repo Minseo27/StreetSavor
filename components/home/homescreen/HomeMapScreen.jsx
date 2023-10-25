0423e1bb026420d6ae3c0df4b7c394df85f028e5
halaway <31904474+halaway@users.noreply.github.com>
2023-10-25 04:45:41 -0400

    Updating Images

--- components/home/homescreen/HomeMapScreen.jsx
+++ components/home/homescreen/HomeMapScreen.jsx
@@ -8,16 +8,26 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 //import Geolocation from '@react-native-community/geolocation';
 // Imports
 //import { AsyncStorage } from 'react-native';
+import { Image } from 'react-native';
+import { Marker } from "react-native-maps";
+import { useNavigation } from "@react-navigation/native";
+import Geolocation from 'react-native-geolocation-service';
 
 
 const HomeMapScreen = ({ navigation }) => {
+
+  //Saving Position 
+  let savedLatitude = null;
+  let savedLongitude = null;
+  
+  
     useEffect(() => {
       checkLocationPermission();
     }, []);
   
     const checkLocationPermission = async () => {
       const locationPermission = await AsyncStorage.getItem('locationPermission');
-      if (locationPermission === 'granted') {
+      if (locationPermission !== 'granted') {
       } else {
         Alert.alert(
           'Location Tracking Permission',
@@ -32,18 +42,42 @@ const HomeMapScreen = ({ navigation }) => {
               onPress: async () => {
                 // Store the user's location
                 // geolocation here
-  
+
+                Geolocation.getCurrentPosition(
+                  (position) => {
+                      //console.log("My current location", JSON.stringify(position));
+                      //this.setState({location: position.coords.latitude.toString() + "," + position.coords.longitude.toString()})
+                      //console.warn(position.coords.latitude)
+                      
+                      savedLatitude = position.coords.latitude;
+                      savedLongitude = position.coords.longitude
+                      
+                  },
+                  (error) => {
+                      // See error code charts below.
+                      console.log(error.code, error.message);
+                  },
+                  { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 });
+
+                  console.warn(savedLatitude)
                 // Store the permission status
-                await AsyncStorage.setItem('locationPermission', 'granted');
+                //await AsyncStorage.setItem('locationPermission', 'granted');
               },
             },
           ]
         );
       }
+
+
+      //console.warn(savedLatitude)
+      
     };
   
+
     return (
+      
       <View style={{ flex: 1, backgroundColor: '#efcb4e' }}>
+       
         <MapView
           // Hunter College Coordinates
           style={{ flex: 1 }}
@@ -54,6 +88,15 @@ const HomeMapScreen = ({ navigation }) => {
             longitudeDelta: 0.02,
           }}
         >
+          <Marker
+          coordinate={{
+            latitude: 40.7861,
+            longitude: -73.9543,
+          }}
+        //image = {require(".asset/mount.jpeg")}
+        
+    
+    />
         </MapView>
   
         <View style={styles.searchContainer}>
