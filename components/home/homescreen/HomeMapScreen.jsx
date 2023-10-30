a5e5d7d6a02e6ca52099cec81a16bcda37c11287
Ying Jie Mei <93160834+jackyx00@users.noreply.github.com>
2023-10-30 12:25:18 -0400

    added geolocation functionality

--- components/home/homescreen/HomeMapScreen.jsx
+++ components/home/homescreen/HomeMapScreen.jsx
@@ -4,11 +4,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { NavigationActions } from 'react-navigation';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import styles from './homemapscreen.styles';
-import MapView from 'react-native-maps';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import MapView, { Marker } from 'react-native-maps';
 import { COLORS, SIZES } from '../../../constants'
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import {Vendor,vendor_list,VendorItem} from '../../../database_vars/vars';
+import * as permissions from 'react-native-permissions';
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import Geolocation from 'react-native-geolocation-service';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 //import { MapsComponent } from '@syncfusion/ej2-react-maps';
 //import Geolocation from '@react-native-community/geolocation';
 // Imports
@@ -102,6 +106,7 @@ const HomeMapScreen = ({navigation}) => {
     )
     */
 
+
 const Tab = createBottomTabNavigator();
 
 function MapScreen() {
@@ -117,6 +122,15 @@ function MapScreen() {
                 longitudeDelta: 0.02,
                 }}
             >
+
+            <Marker
+                coordinate={{
+                latitude: 40.7861, 
+                longitude: -73.9543, 
+                }}
+                title="Initial Location"
+                description="This is the initial location"
+            />
 
             </MapView>
 
@@ -173,6 +187,7 @@ function AccountPage () {
 }
 
 const HomeMapScreen = ({navigation}) => {
+    /*
     useEffect(() => {
         checkLocationPermission();
       }, []);
@@ -203,11 +218,62 @@ const HomeMapScreen = ({navigation}) => {
           );
         }
       };
+      */
 
-    return (
+        const checkLocationPermission = async () => {
+          const result = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+          if (result === RESULTS.DENIED) {
+            const permissionResult = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            if (permissionResult === RESULTS.GRANTED) {
+              getUserLocationAndStoreInFirestore();
+            }
+          } else if (result === RESULTS.GRANTED) {
+            getUserLocationAndStoreInFirestore();
+          }
+        };
+      
+        const getUserLocationAndStoreInFirestore = () => {
+          Geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords;
+              storeLocationInFirestore(latitude, longitude);
+            },
+            (error) => {
+              console.error('Error getting location:', error);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+          );
+        };
+      
+        const storeLocationInFirestore = (latitude, longitude) => {
+        const user = auth().currentUser;
+          firestore()
+            .collection('Users')
+            .doc('dFqhRhGV5BSuqWYys6bP')
+            .collection('Customers')
+            .doc(user.uid)
+            .update({
+              location: {
+                latitude,
+                longitude,
+              },
+            })
+            .then(() => {
+              console.log('Location updated in Firestore');
+            })
+            .catch((error) => {
+              console.error('Error updating location:', error);
+            });
+        };
+      
+        useEffect(() => {
+          checkLocationPermission();
+        }, []);
+
+      return (
         <Tab.Navigator initialRouteName='Map' options={{flex: 1,}}>
             <Tab.Screen name='Map' component={MapScreen} options={{headerShown: false}}/>
-            <Tab.Screen name="Order" component={FirstOrderScreen} options={{headerShown: false}}/>
+            <Tab.Screen name="Cart" component={FirstOrderScreen} options={{headerShown: false}}/>
             <Tab.Screen name="Account" component={AccountPage} options={{headerShown: false}}/>
         </Tab.Navigator>
     );
