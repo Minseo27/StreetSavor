2a244f72eb516f694c111a75294a396893dc9984
halaway <31904474+halaway@users.noreply.github.com>
2023-12-16 23:29:48 -0500

    Removing deprecated community geolocation

--- components/home/homescreen/MapScreen.jsx
+++ components/home/homescreen/MapScreen.jsx
@@ -9,7 +9,7 @@ import { ActivityIndicator } from 'react-native';
 import { Image } from 'react-native';
 import { DirectionsService } from 'react-native-maps-directions';
 import MapViewDirections from 'react-native-maps-directions';
-//import Geolocation from 'react-native-geolocation-service';
+import Geolocation from 'react-native-geolocation-service';
 //import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import { TextInput } from 'react-native-gesture-handler';
 import { TouchableOpacity } from '@gorhom/bottom-sheet';
@@ -17,6 +17,7 @@ import { TouchableOpacity } from '@gorhom/bottom-sheet';
 function MapScreen () {
     const checkLocationPermission = async () => {
         const result = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+        //const result = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
         if (result === RESULTS.DENIED) {
           const permissionResult = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
           if (permissionResult === RESULTS.GRANTED) {
